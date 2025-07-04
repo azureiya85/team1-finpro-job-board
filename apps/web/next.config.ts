62cbd15d1d449b30e25a807e0903c9855b083040
azureiya85 <yosefryan@gmail.com>
2025-07-04 20:44:43 +0700

    fix: suprress console logs in production

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   compiler: {
+    removeConsole: {
+      exclude: ['error'],
+    },
   },
   images: {
     remotePatterns: [
@@ -36,7 +39,6 @@ const nextConfig: NextConfig = {
     ],
   },
   
-  // Add this block to bypass TypeScript errors during the build
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
